998356f459aff76a1e951f2dffda427e41db3410
Jay <jayrathore88155@gmail.com>
2025-12-03 17:04:12 +0530

    Add cors to backend

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express  , {Request , Response} from "express";
+import cors from "cors";
 import { authRouter } from "./routes/auth.routes";
 import { userRouter } from "./routes/user.routes";
 
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended : true}));
+app.use(cors());
 
 app.use("/api/auth" , authRouter);
 app.use("/api/user" , userRouter);
