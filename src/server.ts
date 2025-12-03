ad8b8e0cdc7ec720de1d98e5c48faa79eb1565bd
Jay <jayrathore88155@gmail.com>
2025-12-03 19:43:07 +0530

    add process.env.PORT to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import app from "./app";
 import { connectDB } from "./configs/db.config";
-import dotenv from "dotenv";
-import { config } from "./configs/config";
-
-dotenv.config();
-
-const PORT = config.port;
 
 // Connect DB locally 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
