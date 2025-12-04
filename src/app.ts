89569c9e5e7790d3fcc982a088525f67912c73de
Jay <jayrathore88155@gmail.com>
2025-12-04 13:45:29 +0530

    Add cors header

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cookieParser from "cookie-parser";
-import express  , {Request , Response} from "express";
+import express  , {NextFunction, Request , Response} from "express";
 import cors from "cors";
 import { authRouter } from "./routes/auth.routes";
 import { userRouter } from "./routes/user.routes";
@@ -14,6 +14,11 @@ app.use(cors(
     credentials: true
   }
 ));
+
+app.use((req : Request, res : Response , next : NextFunction) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/api/auth" , authRouter);
 app.use("/api/user" , userRouter);
